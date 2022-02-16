e710e2e48d84c8d77c13ffa6944bb2c8ba23679a
GitHubLhp123 <2446268838@qq.com>
2022-02-16 17:19:42 +0800

    x

---
+++ src/main/java/io/github/githublhp123/util/StrUtil.java
@@ -0,0 +1,12 @@
+package io.github.githublhp123.util;
+
+/**
+ * @description:
+ * @author: lhp
+ * @time: 2022/2/16 5:19 下午
+ */
+public class StrUtil {
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+}
